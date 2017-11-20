6a7badce22c4c572a698c5cad5a4600a8437fc0e
Risto <risto.kekovic@logeecom.com>
2017-11-20 10:17:08 +0100

    Changed API router to router

--- src/Providers/Newsletter2GoRouteServiceProvider.php
+++ src/Providers/Newsletter2GoRouteServiceProvider.php
@@ -12,20 +12,22 @@ use Plenty\Plugin\Routing\ApiRouter;
 class Newsletter2GoRouteServiceProvider extends RouteServiceProvider
 {
 
-/*	public function map(Router $router, ApiRouter $api)
+	public function map(Router $router, ApiRouter $api)
 	{
-        $api->get('newsletter2go/export', 'Newsletter2Go\Controllers\ApiController@export');
-		$api->get('newsletter2go/customers', 'Newsletter2Go\Controllers\ApiController@customers');
-	}*/
+        $router->get('newsletter2go/export', 'Newsletter2Go\Controllers\ApiController@export');
+		$router->get('newsletter2go/customers', 'Newsletter2Go\Controllers\ApiController@customers');
+	}
 
-    public function map(Router $router)
+/*    public function map(Router $router)
     {
         $router->get('newsletter2go/export', [
-            'uses'       => 'Newsletter2Go\Controllers\ApiController@export'
+            'middleware' => 'oauth',
+            'uses'       => 'Newsletter2Go\Controllers\ApiController@export',
         ]);
 
 		$router->get('newsletter2go/customers', [
-			'uses'       => 'Newsletter2Go\Controllers\ApiController@customers'
+			'middleware' => 'oauth',
+			'uses'       => 'Newsletter2Go\Controllers\ApiController@customers',
 		]);
-    }
+    }*/
 }
