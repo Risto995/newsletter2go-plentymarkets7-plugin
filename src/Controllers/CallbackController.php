b55c43ded2af7a2a592f7d70af9a55a364a22cf2
Risto Kekovic <risto.kekovic@logeecom.com>
2018-02-07 14:24:59 +0100

    Remove CallbackConfig, access config table through ConfigRepository

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -2,19 +2,34 @@
 
 namespace Newsletter2Go\Controllers;
 
-use Newsletter2Go\Helpers\CallbackConfig;
+use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Http\Request;
 
 class CallbackController extends Controller
 {
 
+    /**
+     * @var ConfigRepository
+     */
+    private $configRepository;
+
+
+    /**
+     * ApiController constructor.
+     *
+     * @param ConfigRepository $configRepository
+     */
+    public function __construct(ConfigRepository $configRepository)
+    {
+        $this->configRepository = $configRepository;
+    }
+
     public function callback(Request $request)
     {
         $companyId = $request->get('company_id');
         if(isset($companyId)){
-            $callbackConfig = new CallbackConfig();
-            $callbackConfig->set('newsletter2go.company_id', $companyId);
+            $this->configRepository->set('newsletter2go.company_id', $companyId);
 
             return ['success' => true, 'message' => 'OK'];
         } else {
